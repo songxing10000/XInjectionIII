a9f8ba98ef3d2dc8daa05acfc1008ffe3d7b3510
songxing10000 <songxing10000@live.cn>
2022-06-02 22:25:50 +0800

    up

--- XInjectionIII/Classes/UIView+XSRefresh.m
+++ XInjectionIII/Classes/UIView+XSRefresh.m
@@ -6,64 +6,45 @@
 //
 
 #import "UIView+XSRefresh.h"
-#import <objc/runtime.h>
+
 @implementation UIView (XSRefresh)
 -(void)addRealTimeRefreshByAction:(nullable SEL)action {
+#if TARGET_IPHONE_SIMULATOR
     [self addRealTimeRefreshByAction:action controlsNotRemoved:nil];
+#endif
 }
 -(void)addRealTimeRefreshByAction:(nullable SEL)action controlsNotRemoved:(nullable NSArray<UIView *> *)controlsNotRemoved {
-
 #if TARGET_IPHONE_SIMULATOR
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(injectionNotification:) name:@"INJECTION_BUNDLE_NOTIFICATION" object:nil];
-    [self class].action = action;
+    xs_action = action;
     if (controlsNotRemoved.count > 0) {
-        self.controlsNotRemoved = controlsNotRemoved;
+        xs_controlsNotRemoved = controlsNotRemoved;
     }
 #endif
-    
 }
 
 #if TARGET_IPHONE_SIMULATOR
-static SEL _action = nil;
-+ (void)setAction:(SEL)action {
-  if (_action != action) {
-      _action = action;
-  }
-}
-+(SEL)action {
-    return _action;
-}
-//@property(nonatomic, strong) NSArray<UIView *>  *controlsNotRemoved;
-- (void)setControlsNotRemoved:(NSArray<UIView *> *)controlsNotRemoved {
-    objc_setAssociatedObject(self, @selector(controlsNotRemoved), controlsNotRemoved, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-- (NSArray<UIView *> *)controlsNotRemoved {
-    return objc_getAssociatedObject(self, _cmd);
-}
-
+static SEL xs_action = nil;
+/// 刷新时不从父控件上移除的控件
+static NSArray<UIView *> *xs_controlsNotRemoved = nil;
 
 - (void)injectionNotification:(NSNotification *)notification {
     
     NSArray *array = notification.object;
-    
-    for (id obj in array) {
-        
+    [array enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         if ([self isKindOfClass:obj]) {
             
             if ([self isKindOfClass:[UITableViewCell class]]) {
                 
                 UITableViewCell *cell = (UITableViewCell *)self;
-                
                 [cell.contentView.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
                     [obj removeFromSuperview];
                 }];
-                
             }
             
             else if ([self isKindOfClass:[UICollectionViewCell class]]) {
                 
                 UICollectionViewCell *cell = (UICollectionViewCell *)self;
-                
                 [cell.contentView.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
                     [obj removeFromSuperview];
                 }];
@@ -71,33 +52,49 @@ static SEL _action = nil;
             else if ([self isKindOfClass:[UITableViewHeaderFooterView class]]) {
                 
                 UITableViewHeaderFooterView *hfv = (UITableViewHeaderFooterView *)self;
-                
                 [hfv.contentView.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
                     [obj removeFromSuperview];
                 }];
             }
-            
-            
             else {
                 [self.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                    if(![self.controlsNotRemoved containsObject:obj]) {
+                    if(![xs_controlsNotRemoved containsObject:obj]) {
                         [obj removeFromSuperview];
                     } else {
-                        [obj.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                        [obj.subviews  enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
                             [obj removeFromSuperview];
                         }];
                     }
                 }];
-                
             }
-            [self performSelector:[self class].action];
-            
+            [self performSelector:xs_action];
         }
-        
-    }
-    
+    }];
 }
 #endif
- 
+
 @end
 
+/*
+ 将SEL转换为NSString
+ - (void)setAction:(SEL)action {
+     objc_setAssociatedObject(self, "k_ref_action", NSStringFromSelector(action), OBJC_ASSOCIATION_COPY_NONATOMIC);
+ }
+ - (SEL)action {
+     return NSSelectorFromString(objc_getAssociatedObject(self, "k_ref_action"));
+ }
+
+ 将SEL转换为NSValue
+
+ - (void)setAction:(SEL)action {
+     NSValue *selValue = [NSValue valueWithBytes:&action objCType:@encode(SEL)];
+     objc_setAssociatedObject(self, "k_ref_action", selValue, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+ }
+ - (SEL)action {
+     NSValue *av = objc_getAssociatedObject(self, "k_ref_action");
+     SEL as;
+     [av getValue:&as];
+     return as;
+ }
+ 
+ */
